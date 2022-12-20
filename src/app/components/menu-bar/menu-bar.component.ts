dadc1a3bfdb15413a5b7666ce97b666758387122
Gabriel <gabpnavarro@hotmail.com>
2022-12-20 04:11:35 -0300

    main

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: [
+    './menu-bar.component.css',
+    './menu-bar.responsive.component.css'
+  ]
 })
 export class MenuBarComponent implements OnInit {
 
