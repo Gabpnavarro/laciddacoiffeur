efc74f242289dfaf93f80fb736515f076a72fa42
Gabriel <gabpnavarro@hotmail.com>
2022-12-22 19:28:32 -0300

    main

--- src/app/components/service-photos/service-photos.component.ts
+++ src/app/components/service-photos/service-photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-service-photos',
@@ -10,6 +10,20 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ServicePhotosComponent implements OnInit {
+  @Input()
+  photoLeft:string= ""
+  @Input()
+  photoCenter:string= ""
+  @Input()
+  photoRight:string= ""
+
+  @Input()
+  titlePhotoLeft:string=""
+  @Input()
+  titlePhotoCenter:string=""
+  @Input()
+  titlePhotoRight:string=""
+
 
   constructor() { }
 
