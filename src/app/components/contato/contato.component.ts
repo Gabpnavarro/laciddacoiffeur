c5b4f722ccfae9e10fbf14a2c76ae16164f1dbf2
Gabriel <gabpnavarro@hotmail.com>
2022-12-23 01:08:24 -0300

    main

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contato',
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ContatoComponent implements OnInit {
+
+  @Input()
+  cardTitle:string = ""
+ @Input()
+  cardNumber:string = ""
+
 
   constructor() { }
 
--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
@@ -9,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MenuBarComponent implements OnInit {
-
+  @Input()
+  cardHome:string=""
+  @Input()
+  cardBemvindo:string=""
+  @Input()
+  cardServicos:string=""
+  @Input()
+  cardContato:string=""
+  
   constructor() { }
 
   ngOnInit(): void {
